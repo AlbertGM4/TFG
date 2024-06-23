42ad9dad2d3045e49b1b5e2c01dca6d5ac011a52
Albert <albert.gonzalezmoral@gmail.com>
2024-06-23 14:01:40 +0200

    Changes api url

--- app/services/ApiASP.tsx
+++ app/services/ApiASP.tsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5188', // o https://localhost:7037 si prefieres usar HTTPS
-    withCredentials: true // Asegura que las cookies se envíen con cada solicitud
+    baseURL: 'https://tejedorapi.azure-api.net',
+    withCredentials: true
 });
 
 export default api;
