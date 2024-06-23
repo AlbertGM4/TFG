83ff1a2ad79d11799b0eca7942f58a1ab170336f
Albert <albert.gonzalezmoral@gmail.com>
2024-06-23 14:47:34 +0200

    Changed url

--- app/services/ApiASP.tsx
+++ app/services/ApiASP.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://tejedorapi.azure-api.net',
+    baseURL: 'https://tejedorapi.azurewebsites.net/',
     withCredentials: true
 });
 
